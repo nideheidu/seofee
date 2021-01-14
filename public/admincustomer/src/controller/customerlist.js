a4f67bc595ddbf31c1299c2aae57d5777a9578c0
dumaolin <1327317790@qq.com>
2021-01-14 08:48:39 +0800

    seo计费系统管理后台

---
+++ public/admincustomer/src/controller/customerlist.js
@@ -0,0 +1,151 @@
+/**
+ * 客户
+ */
+
+
+layui.define(['table', 'form','setter'], function(exports){
+    var $ = layui.$
+        ,admin = layui.admin
+        ,view = layui.view
+        ,table = layui.table
+        ,setter = layui.setter
+        ,request = setter.request
+        ,form = layui.form;
+
+    //客户列表
+    table.render({
+        elem: '#LAY-app-customer-list'
+        ,url: '/admin/customer/list'
+        ,headers:{
+            'access-token':(layui.data(setter.tableName)[request.tokenName] || '')
+        }
+        ,cols: [[
+            {type: 'checkbox', fixed: 'left', align: 'center'}
+            ,{field: 'username', title: '客户账号', minWidth: 100, align: 'center'}
+            ,{field: 'contacts', title: '联系人', minWidth: 100, align: 'center'}
+            ,{field: 'qq_number', title: 'QQ', align: 'center'}
+            ,{field: 'email', title: '邮箱', align: 'center'}
+            ,{field: 'company_name', title: '公司名称',minWidth: 100, align: 'center'}
+            ,{field: 'order_count', title: '任务数量',sort: true, align: 'center'}
+            ,{field: 'compliance_rate', title: '达标率',sort: true, align: 'center'}
+            ,{field: 'balance', title: '余额', sort: true, align: 'center'}
+           
+            ,{field: 'lev', title: '会员等级'}
+            ,{field: 'last_login_time', title: '最近登录',minWidth: 200, align: 'center'}
+            ,{field: 'create_time', title: '注册时间', sort: true, minWidth: 200, align: 'center'}
+            ,{field: 'status', title: '状态', templet: '#buttonTpl', minWidth: 80, align: 'center'}
+            ,{title: '操作', minWidth: 250, align: 'center', fixed: 'right', toolbar: '#table-content-list', align: 'center'}
+        ]]
+        ,parseData:function ( res ) {
+            return {
+                "code": res.code, //解析接口状态
+                "msg": res.msg, //解析提示文本
+                "count": res.data.total,  //解析数据长度
+                "data": res.data.data.length>0? res.data.data:null //解析数据列表
+            };
+        }
+        ,page: true
+        ,limit: 30
+        ,limits: [30, 50, 100, 200, 500]
+        ,text: {none: '一条数据也没有^_^'}
+    });
+
+    //监听工具条
+    table.on('tool(LAY-app-customer-list)', function(obj){
+        var data = obj.data;
+        if(obj.event === 'editBalance'){
+            var params ={}
+            var flag = true;
+            var html = '<div class="layui-form-item">\n' +
+                '        <label class="layui-form-label">修改金额</label>\n' +
+                '        <div class="layui-input-inline">\n' +
+                '                <input type="number" name="balance" id="balanceInput" value="0" lay-verify="required" autocomplete="off" class="layui-input">\n' +
+                '        </div>\n' +
+                '        <div class="layui-form-mid layui-word-aux">\n' +
+                '            <span style="color: red">* 正数为增加，负数为减少</span>'+
+                '        </div>\n' +
+                '    </div>'
+            var baindex = layer.open({
+                type: 1
+                ,title:'修改余额'
+                ,id: 'edit-balance' //防止重复弹出
+                ,content: '<div class="layui-form" style="padding: 50px 20px 0px 20px;">'+ html +'</div>'
+                ,btn: ['提交','取消']
+                ,btnAlign: 'c' //按钮居中
+                ,shade: 0 //不显示遮罩
+                ,yes: function(){
+                    params.uid = data.uid;
+                    params.balance = $("#balanceInput").val();
+                    //alert(params.balance);return;
+                    if(params.balance ==='0'){
+                        layer.close(baindex);
+                        return;
+                    }
+                    if(!flag){
+                        return false;
+                    }
+                    flag = false;
+                    admin.req({
+                            url: '/admin/customer/editbalance'
+                            ,type:"post"
+                            ,data:params
+                            ,success:function () {
+                            }
+                            ,done: function( res ){
+                                layer.msg(res.msg, {
+                                    offset: '15px'
+                                    ,icon: 1
+                                    ,time: 1000
+                                },function () {
+                                    layui.table.reload('LAY-app-customer-list'); //重载表格
+                                    layer.close(baindex); //执行关闭
+                                });
+                            }
+                        });
+
+                }
+            });
+        } else if(obj.event === 'edit'){
+            admin.popup({
+                title: '编辑客户'
+                ,area: ['700px', '600px']
+                ,id: 'LAY-popup-customer-edit'
+                ,success: function(layero, index){
+                    console.log(data);
+                    view(this.id).render('seo/customer/edit', data).done(function(){
+                        form.render(null, 'layuiadmin-app-form-list');
+
+                        //监听提交
+                        form.on('submit(layuiadmin-seo-customer-edit-submit)', function(data){
+                            var field = data.field; //获取提交的字段
+                            if(field.status === "on"){
+                                field.status = 1;
+                            }else {
+                                field.status = 0;
+                            }
+                            admin.req({
+                                url: '/admin/customer/edit'
+                                ,type:"post"
+                                ,data:field
+                                ,done: function( res ){
+                                    //登入成功的提示与跳转
+                                    layer.msg(res.msg, {
+                                        offset: '15px'
+                                        ,icon: 1
+                                        ,time: 1000
+                                    },function () {
+                                        layui.table.reload('LAY-app-customer-list'); //重载表格
+                                        //layer.close(index); //执行关闭
+                                    });
+                                }
+                            });
+                        });
+                    });
+                }
+            });
+        }
+    });
+
+
+    exports('customerlist', {})
+});
---
+++ public/admincustomer/src/controller/sample.js
@@ -0,0 +1,246 @@
+/**
+
+ @Name：layuiAdmin 主页示例
+ @Author：star1029
+ @Site：http://www.layui.com/admin/
+ @License：GPL-2
+    
+ */
+
+
+layui.define(function(exports){
+  var admin = layui.admin;
+
+  //区块轮播切换
+  layui.use(['admin', 'carousel'], function(){
+    var $ = layui.$
+    ,admin = layui.admin
+    ,carousel = layui.carousel
+    ,element = layui.element
+    ,device = layui.device();
+
+    //轮播切换
+    $('.layadmin-carousel').each(function(){
+      var othis = $(this);
+      carousel.render({
+        elem: this
+        ,width: '100%'
+        ,arrow: 'none'
+        ,interval: othis.data('interval')
+        ,autoplay: othis.data('autoplay') === true
+        ,trigger: (device.ios || device.android) ? 'click' : 'hover'
+        ,anim: othis.data('anim')
+      });
+    });
+
+    element.render('progress');
+
+  });
+
+  //八卦新闻
+  layui.use(['carousel', 'echarts'], function(){
+    var $ = layui.$
+    ,carousel = layui.carousel
+    ,echarts = layui.echarts;
+
+    var echartsApp = [], options = [
+      {
+        title : {
+          subtext: '完全实况球员数据',
+          textStyle: {
+            fontSize: 14
+          }
+        },
+        tooltip : {
+          trigger: 'axis'
+        },
+        legend: {
+          x : 'left',
+          data:['罗纳尔多','舍普琴科']
+        },
+        polar : [
+          {
+            indicator : [
+              {text : '进攻', max  : 100},
+              {text : '防守', max  : 100},
+              {text : '体能', max  : 100},
+              {text : '速度', max  : 100},
+              {text : '力量', max  : 100},
+              {text : '技巧', max  : 100}
+            ],
+            radius : 130
+          }
+        ],
+        series : [
+          {
+            type: 'radar',
+            center : ['50%', '50%'],
+            itemStyle: {
+              normal: {
+                areaStyle: {
+                  type: 'default'
+                }
+              }
+            },
+            data:[
+              {value : [97, 42, 88, 94, 90, 86], name : '舍普琴科'},
+              {value : [97, 32, 74, 95, 88, 92], name : '罗纳尔多'}
+            ]
+          }
+        ]
+      }
+    ]
+    ,elemDataView = $('#LAY-index-pageone').children('div')
+    ,renderDataView = function(index){
+      echartsApp[index] = echarts.init(elemDataView[index], layui.echartsTheme);
+      echartsApp[index].setOption(options[index]);
+      window.onresize = echartsApp[index].resize;
+    };
+    //没找到DOM，终止执行
+    if(!elemDataView[0]) return;
+
+    renderDataView(0);
+  });
+
+  //统计
+  layui.use(['carousel', 'echarts'], function(){
+    var $ = layui.$
+    ,carousel = layui.carousel
+    ,echarts = layui.echarts
+    ,nextmonth = []
+    ,thismonth = [];
+    admin.req({
+          url: '/v1/task/getcounts'
+          ,type:"post"
+          ,success:function (res) {
+              
+          }
+          ,done: function( res ){
+              console.log(res);
+                nextmonth=res.nextmonth;
+                thismonth=res.thismonth;
+              
+              //堆积折线图
+              var echheapline = [], heapline = [
+                  {
+                      tooltip : {
+                          trigger: 'axis'
+                      },
+                      legend: {data:['本月达标','上月达标']},
+                      calculable : true,
+                      xAxis : [
+                          {
+                              type : 'category',
+                              boundaryGap : false,
+                              data : ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31']
+                          }
+                      ],
+                      yAxis : [
+                          {
+                              type : 'value'
+                          }
+                      ],
+                      series : [
+                          {
+                              name:'本月达标',
+                              type:'line',
+                              smooth: true, seriesLayoutBy: 'row',
+                              data:thismonth
+                          },
+                          {
+                              name:'上月达标',
+                              type:'line',
+                              smooth: true, seriesLayoutBy: 'row',
+                              data:nextmonth
+                          }
+                      ]
+                  }
+              ]
+                  ,elemheapline = $('#LAY-index-taskline').children('div')
+                  ,renderheapline = function(index){
+                  echheapline[index] = echarts.init(elemheapline[index], layui.echartsTheme);
+                  echheapline[index].setOption(heapline[index]);
+                  window.onresize = echheapline[index].resize;
+              };
+              if(!elemheapline[0]) return;
+                renderheapline(0);
+          }
+      });
+      //提交审核
+    // $.ajax({
+    //   url: '/v1/task/getcounts' 
+    //   ,type: 'get'
+    //   ,dataType: 'html'
+    //   ,success: function(res){
+    //     res=$.parseJSON(res);
+    //     nextmonth=res.nextmonth;
+    //     thismonth=res.thismonth;
+      
+    //   //堆积折线图
+    //   var echheapline = [], heapline = [
+    //       {
+    //           tooltip : {
+    //               trigger: 'axis'
+    //           },
+    //           legend: {data:['本月达标','上月达标']},
+    //           calculable : true,
+    //           xAxis : [
+    //               {
+    //                   type : 'category',
+    //                   boundaryGap : false,
+    //                   data : ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31']
+    //               }
+    //           ],
+    //           yAxis : [
+    //               {
+    //                   type : 'value'
+    //               }
+    //           ],
+    //           series : [
+    //               {
+    //                   name:'本月达标',
+    //                   type:'line',
+    //                   smooth: true, seriesLayoutBy: 'row',
+    //                   data:thismonth
+    //               },
+    //               {
+    //                   name:'上月达标',
+    //                   type:'line',
+    //                   smooth: true, seriesLayoutBy: 'row',
+    //                   data:nextmonth
+    //               }
+    //           ]
+    //       }
+    //   ]
+    //       ,elemheapline = $('#LAY-index-taskline').children('div')
+    //       ,renderheapline = function(index){
+    //       echheapline[index] = echarts.init(elemheapline[index], layui.echartsTheme);
+    //       echheapline[index].setOption(heapline[index]);
+    //       window.onresize = echheapline[index].resize;
+    //   };
+    //   if(!elemheapline[0]) return;
+    //     renderheapline(0);
+    //   }
+    //   ,error: function(e){
+    //   }
+    // });
+      
+    
+  });
+
+  //回复留言
+  admin.events.replyNote = function(othis){
+    var nid = othis.data('id');
+    layer.prompt({
+      title: '回复留言 ID:'+ nid
+      ,formType: 2
+    }, function(value, index){
+      //这里可以请求 Ajax
+      //…
+      layer.msg('得到：'+ value);
+      layer.close(index);
+    });
+  };
+
+  exports('sample', {})
+});
